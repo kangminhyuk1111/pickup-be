fd7d37f82007f5f98ee23f005288361aea2b1515
강민혁 <rkdglqkr12@naver.com>
2025-06-05 11:25:10 +0900

    refactor: env 세팅 변경

--- src/test/java/com/example/shoppingmall/ShopApplicationTests.java
+++ src/test/java/com/example/shoppingmall/ShopApplicationTests.java
@@ -5,25 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-		"KAKAO_CLIENT_ID=test-kakao-client-id",
-		"KAKAO_CLIENT_SECRET=test-kakao-client-secret",
-		"KAKAO_REDIRECT_URI=http://localhost:8080/test/auth/callback/kakao",
-
-		"OAUTH_GITHUB_CLIENT_ID=test-github-client-id",
-		"OAUTH_GITHUB_CLIENT_SECRET=test-github-client-secret",
-		"OAUTH_GITHUB_REDIRECT_URI=http://localhost:8080/test/auth/callback/github",
-
-		"GOOGLE_CLIENT_ID=test-google-client-id",
-		"GOOGLE_CLIENT_SECRET=test-google-client-secret",
-		"GOOGLE_REDIRECT_URI=http://localhost:8080/test/auth/callback/google",
-
-		"JWT_SECRET_KEY=testSecretKeyForTestingPurposesOnly123456789012345678901234567890"
-})
 @Import(TestAuthConfig.class)
 class ShopApplicationTests {
 
